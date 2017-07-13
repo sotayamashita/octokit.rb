47354f808eb806d5682f5225850a4aa6c43608bf
Sota Yamashita <sota.yamashita@gmail.com>
2017-07-13 23:39:43 +0900

    Upda

--- lib/octokit/client/apps.rb
+++ lib/octokit/client/apps.rb
@@ -142,9 +142,21 @@ module Octokit
       # @see https://developer.github.com/v3/apps/marketplace/#list-all-plans-for-your-marketplace-listing
       #
       # @return  [Array<Sawyer::Resource>] A list of marketplace
-      def list_marketplace_plans(options = {})
+      def find_app_marketplace_plans(options = {})
         opts = ensure_api_media_type(:marketplace, options)
         get "/marketplace_listing/plans", opts
+      end
+
+      # Get a user's Marketplace purchases
+      #
+      # @param options [Hash] An customizable set of options
+      #
+      # @see https://developer.github.com/v3/apps/marketplace/#get-a-users-marketplace-purchases
+      #
+      # @return [Array<Sawyer::Resource>] A list of a user's Marketplace purchases
+      def find_app_marketplace_purchases(options = {})
+        opts = ensure_api_media_type(:marketplace, options)
+        get "/user/marketplace_purchases", opts
       end
     end
   end
