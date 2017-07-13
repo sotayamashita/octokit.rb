f9eb7f4572bb5c5007b6297c58a0e80d3c01520d
Sota Yamashita <sota.yamashita@gmail.com>
2017-07-13 23:42:32 +0900

    Add marketplace

---
+++ lib/octokit/client/marketplace.rb
@@ -0,0 +1,32 @@
+module Octokit
+  class Client
+
+    # Methods for the Apps API
+    module Marketplace
+
+      # List all plans for your Marketplace listing
+      #
+      # @param options [Hash] An customizable set of options
+      #
+      # @see https://developer.github.com/v3/apps/marketplace/#list-all-plans-for-your-marketplace-listing
+      #
+      # @return  [Array<Sawyer::Resource>] A list of marketplace
+      def find_app_marketplace_plans(options = {})
+        opts = ensure_api_media_type(:marketplace, options)
+        get "/marketplace_listing/plans", opts
+      end
+
+      # Get a user's Marketplace purchases
+      #
+      # @param options [Hash] An customizable set of options
+      #
+      # @see https://developer.github.com/v3/apps/marketplace/#get-a-users-marketplace-purchases
+      #
+      # @return [Array<Sawyer::Resource>] A list of a user's Marketplace purchases
+      def find_app_marketplace_purchases(options = {})
+        opts = ensure_api_media_type(:marketplace, options)
+        get "/user/marketplace_purchases", opts
+      end
+    end
+  end
+end
